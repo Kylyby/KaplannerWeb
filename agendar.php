d1b1e8fb2f5c4c2dbba827fe1aad98c9e95b3a35
Kylyby <phximenes38@gmail.com>
2023-11-23 19:11:47 -0400

    css do cadastro finalizado?

--- agendar.php
+++ agendar.php
@@ -9,37 +9,47 @@
 <body>
 
     <fieldset id="form_field">
-        <legend>Agendar evento</legend>
-        <form action="get" id="formulario">
+        <form method="get" id="formulario">
+            <br><label id="title_form">Agendar evento</label>
             <div id="div_form">
                 <div id="form1">
                     <div id="titulo_div">
-                        <label>Título</label><br>
-                        <input type="text">
+                        <label>Título</label><br><br>
+                        <input type="text" class="large_inp">
                     </div>
                     <br><br>
                     <div id="desc_div">
-                        <label>Descrição</label><br>
-                        <textarea cols="20" rows="3" placeholder="Meu super duper evento"></textarea>
+                        <label>Descrição</label><br><br>
+                        <textarea cols="20" rows="5" placeholder="Meu super duper evento" class="large_inp"></textarea>
                     </div>
                 </div>
 
                 <div id="form2">
                     <div id="data_div">
-                        <label>Data</label><br>
+                        <label>Data</label><br><br>
                         <input type="date">
                     </div>
                     <br><br>
                     <div id="hora_div">
-                        <label>Hora</label><br>
+                        <label>Hora</label><br><br>
                         <input type="time">
                     </div>
                 </div>
             </div>
 
-            <input type="submit">
+            <br><br>
+            <div id="div_submit">
+                <input type="submit" id="submit">
+                <button type="button" id="voltar" onclick="goBack()">Cancelar</button>
+            </div>
         </form>
     </fieldset>
+
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
 
     <?php
 
