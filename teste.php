48bceeb7951a8163bb7fc7f7e547857420c7a096
Kylyby <phximenes38@gmail.com>
2023-11-23 23:32:03 -0400

    como é bom casca de parede

---
+++ teste.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>teste</title>
+</head>
+
+<body>
+    aaaaaaaaaaaaaaaaa
+
+    <?php
+
+    $conexao = mysqli_connect("localhost:3306", "root", "segredo 🤫");
+
+    if (mysqli_connect_errno()) {
+        echo "Falha ao conectar com o MySQL: " . mysqli_connect_error();
+    } else {
+        echo "Sucesso";
+    }
+    ?>
+</body>
+
+</html>
